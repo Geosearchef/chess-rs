974e3a0342a514bf4420949a3ebc5dd82935b2db
Geosearchef <geosearchef@geosearchef.de>
2025-06-06 22:38:33 +0200

    rust is cool

--- src/chess/board.rs
+++ src/chess/board.rs
@@ -1,53 +1,107 @@
-use std::fmt::Display;
+use std::{fmt::Display, num::NonZeroU8};
+use itertools::Itertools;
 
 use crate::chess::Coord;
 
-const COLOR_WHITE: u8 = 0b0000_0001;
-const COLOR_BLACK: u8 = 0b0000_0010;
-const TYPE_PAWN: u8 = 0b0000_0100;
-const TYPE_KNIGHT: u8 = 0b0000_1000;
-const TYPE_BISHOP: u8 = 0b0001_0000;
-const TYPE_ROOK: u8 = 0b0010_0000;
-const TYPE_QUEEN: u8 = 0b0100_0000;
-const TYPE_KING: u8 = 0b1000_0000;
+const PIECE_TYPE_MASK: u8 = 0b0000_0111;
+const PIECE_COLOR_MASK: u8 = 0b0000_1000;
 
-const SQUARE_EMPTY: u8 = 0;
+#[repr(u8)]
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum Color {
+    White = PIECE_COLOR_MASK * 0,
+    Black = PIECE_COLOR_MASK * 1,
+}
 
-pub struct Piece(u8);
+impl From<u8> for Color {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => Self::White,
+            PIECE_COLOR_MASK => Self::Black,
+            _ => unreachable!()
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum PieceType {
+    Pawn = 1,
+    Knight = 2,
+    Bishop = 3,
+    Rook = 4,
+    Queen = 5,
+    King = 6,
+}
+
+impl From<u8> for PieceType {
+    fn from(value: u8) -> Self {
+        match value {
+            1 => Self::Pawn,
+            2 => Self::Knight,
+            3 => Self::Bishop,
+            4 => Self::Rook,
+            5 => Self::Queen,
+            6 => Self::King,
+            _ => unreachable!()
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct Piece(NonZeroU8);
 
 impl Piece {
-    pub fn new(color: u8, ptype: u8) -> Self {
-        Self(color | ptype)
+    pub fn new(color: Color, piece_type: PieceType) -> Self {
+        unsafe {
+            Self(NonZeroU8::new_unchecked(color as u8 | piece_type as u8))
+        }
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.0 == SQUARE_EMPTY
+    #[inline]
+    pub fn color_code(&self) -> u8 {
+        self.0.get() & PIECE_COLOR_MASK
+    }
+
+    #[inline]
+    pub fn piece_type_code(&self) -> u8 {
+        self.0.get() & PIECE_TYPE_MASK
+    }
+
+    #[inline]
+    pub fn color(&self) -> Color {
+        self.color_code().into()
+    }
+
+    #[inline]
+    pub fn piece_type(&self) -> PieceType {
+        self.piece_type_code().into()
     }
 
     pub fn is_white(&self) -> bool {
-        (self.0 & COLOR_WHITE) != 0
+        matches!(self.color(), Color::White)
     }
     pub fn is_black(&self) -> bool {
-        (self.0 & COLOR_BLACK) != 0
+        matches!(self.color(), Color::Black)
     }
 
     pub fn is_pawn(&self) -> bool {
-        (self.0 & TYPE_PAWN) != 0
+        matches!(self.piece_type(), PieceType::Pawn)
     }
     pub fn is_knight(&self) -> bool {
-        (self.0 & TYPE_KNIGHT) != 0
+        matches!(self.piece_type(), PieceType::Knight)
     }
     pub fn is_bishop(&self) -> bool {
-        (self.0 & TYPE_BISHOP) != 0
+        matches!(self.piece_type(), PieceType::Bishop)
     }
     pub fn is_rook(&self) -> bool {
-        (self.0 & TYPE_ROOK) != 0
+        matches!(self.piece_type(), PieceType::Rook)
     }
     pub fn is_queen(&self) -> bool {
-        (self.0 & TYPE_QUEEN) != 0
+        matches!(self.piece_type(), PieceType::Queen)
     }
     pub fn is_king(&self) -> bool {
-        (self.0 & TYPE_KING) != 0
+        matches!(self.piece_type(), PieceType::King)
     }
 }
 
@@ -56,7 +110,7 @@ impl Piece {
 type BoardIndex = u8;
 
 pub struct Board {
-    squares: [Piece; 64],
+    squares: [[Option<Piece>; 8]; 8], // encoded as Option<Piece<NonZeroU8>> which is u8 with 0 representing None, Rust is cool
     white_king_move: bool,
     black_king_move: bool,
     white_castled: bool,
@@ -64,35 +118,39 @@ pub struct Board {
 }
 
 impl Board {
-    pub fn index(coord: Coord) -> BoardIndex {
-        coord.0 + coord.1 * 8
+    pub fn piece_at(&self, coord: Coord) -> &Option<Piece> {
+        &self.squares[coord.1 as usize][coord.0 as usize]
     }
 
-    pub fn coords(index: BoardIndex) -> Coord {
-        (index % 8, index / 8)
-    }
-
-    pub fn piece_at(&self, coord: Coord) -> &Piece {
-        &self.squares[Board::index(coord) as usize]
-    }
-
-    pub fn piece_at_mut(&mut self, coord: Coord) -> &mut Piece {
-        &mut self.squares[Board::index(coord) as usize]
+    pub fn piece_at_mut(&mut self, coord: Coord) -> &mut Option<Piece> {
+        &mut self.squares[coord.1 as usize][coord.0 as usize]
     }
 }
 
 impl Default for Board {
     fn default() -> Self {
+        use Color::*;
+        use PieceType::*;
+
+        let base_row = |color| {
+            [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook].map(|t| Some(Piece::new(color, t)))
+        };
+        let pawn_row = |color| {
+            [Pawn; 8].map(|t| Some(Piece::new(color, t)))
+        };
+        let empty_row = [None; 8];
+
         let squares = [
-            Piece::new(COLOR_BLACK, TYPE_ROOK), Piece::new(COLOR_BLACK, TYPE_KNIGHT), Piece::new(COLOR_BLACK, TYPE_BISHOP), Piece::new(COLOR_BLACK, TYPE_QUEEN), Piece::new(COLOR_BLACK, TYPE_KING), Piece::new(COLOR_BLACK, TYPE_BISHOP), Piece::new(COLOR_BLACK, TYPE_KNIGHT), Piece::new(COLOR_BLACK, TYPE_ROOK),
-            Piece::new(COLOR_BLACK, TYPE_PAWN), Piece::new(COLOR_BLACK, TYPE_PAWN), Piece::new(COLOR_BLACK, TYPE_PAWN), Piece::new(COLOR_BLACK, TYPE_PAWN), Piece::new(COLOR_BLACK, TYPE_PAWN), Piece::new(COLOR_BLACK, TYPE_PAWN), Piece::new(COLOR_BLACK, TYPE_PAWN), Piece::new(COLOR_BLACK, TYPE_PAWN),
-            Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY),
-            Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY),
-            Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY),
-            Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY), Piece(SQUARE_EMPTY),
-            Piece::new(COLOR_WHITE, TYPE_PAWN), Piece::new(COLOR_WHITE, TYPE_PAWN), Piece::new(COLOR_WHITE, TYPE_PAWN), Piece::new(COLOR_WHITE, TYPE_PAWN), Piece::new(COLOR_WHITE, TYPE_PAWN), Piece::new(COLOR_WHITE, TYPE_PAWN), Piece::new(COLOR_WHITE, TYPE_PAWN), Piece::new(COLOR_WHITE, TYPE_PAWN),
-            Piece::new(COLOR_WHITE, TYPE_ROOK), Piece::new(COLOR_WHITE, TYPE_KNIGHT), Piece::new(COLOR_WHITE, TYPE_BISHOP), Piece::new(COLOR_WHITE, TYPE_QUEEN), Piece::new(COLOR_WHITE, TYPE_KING), Piece::new(COLOR_WHITE, TYPE_BISHOP), Piece::new(COLOR_WHITE, TYPE_KNIGHT), Piece::new(COLOR_WHITE, TYPE_ROOK),
+            base_row(Black),
+            pawn_row(Black),
+            empty_row,
+            empty_row,
+            empty_row,
+            empty_row,
+            pawn_row(White),
+            base_row(White),
         ];
+        
         Self {
             squares,
             white_king_move: false,
@@ -108,63 +166,44 @@ impl Default for Board {
 
 impl Display for Piece {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.is_empty() {
-            write!(f, " ")?;
-        } else if self.is_white() {
-            if self.is_pawn() {
-                write!(f, "♙")?;
-            } else if self.is_knight() {
-                write!(f, "♘")?;
-            } else if self.is_bishop() {
-                write!(f, "♗")?;
-            } else if self.is_rook() {
-                write!(f, "♖")?;
-            } else if self.is_queen() {
-                write!(f, "♕")?;
-            } else if self.is_king() {
-                write!(f, "♔")?;
-            } else {
-                write!(f, "?")?;
+        let char = if self.is_white() {
+            match self.piece_type() {
+                PieceType::Pawn => "♙",
+                PieceType::Knight => "♘",
+                PieceType::Bishop => "♗",
+                PieceType::Rook => "♖",
+                PieceType::Queen => "♕",
+                PieceType::King => "♔",
             }
         } else if self.is_black() {
-            if self.is_pawn() {
-                write!(f, "♟")?;
-            } else if self.is_knight() {
-                write!(f, "♞")?;
-            } else if self.is_bishop() {
-                write!(f, "♝")?;
-            } else if self.is_rook() {
-                write!(f, "♜")?;
-            } else if self.is_queen() {
-                write!(f, "♛")?;
-            } else if self.is_king() {
-                write!(f, "♚")?;
-            } else {
-                write!(f, "?")?;
+            match self.piece_type() {
+                PieceType::Pawn => "♟",
+                PieceType::Knight => "♞",
+                PieceType::Bishop => "♝",
+                PieceType::Rook => "♜",
+                PieceType::Queen => "♛",
+                PieceType::King => "♚",
             }
         } else {
-            write!(f, "?")?;
-        }
+            "?"
+        };
 
-        Ok(())
+        write!(f, "{char}")
     }
 }
 
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for y in 0u8..8u8 {
-            for x in 0u8..8u8 {
-                write!(f, "{}", self.piece_at((x, y)))?;
-                if x != 7 {
-                    write!(f, " ")?;
+        let str = self.squares.iter().map(|row| {
+            row.iter().map(|square| {
+                match square {
+                    Some(p) => p.to_string(),
+                    None => " ".to_string(),
                 }
-            }
-            if y != 7 {
-                write!(f, "\n")?;
-            }
-        }
+            }).join(" ")
+        }).join("\n");
 
-        Ok(())
+        write!(f, "{str}")
     }
 }
 
