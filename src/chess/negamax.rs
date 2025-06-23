4745471cdef1eb228c17524b28f6643768f6b5ea
geosearchef <geosearchef@geosearchef.de>
2025-06-23 23:50:10 +0200

    alpha beta pruning

--- src/chess/negamax.rs
+++ src/chess/negamax.rs
@@ -1,7 +1,6 @@
 use crate::chess::board::Board;
-use crate::chess::board::Color::White;
 use crate::chess::r#move::Move;
-use crate::chess::transposition::TranspositionTable;
+use crate::chess::transposition::{EntryType, TranspositionTable};
 use crate::chess::zobrist::ZobristTable;
 
 use rayon::prelude::*;
@@ -22,31 +21,51 @@ pub fn negamax_move(board: Board, depth: u8, zobrist_table: &ZobristTable) -> Op
         let mut updated_board = board.clone();
         updated_board.execute_move(r#move, zobrist_table);
 
-        (r#move, -negamax(updated_board, depth - 1, zobrist_table, &mut ctx)) // clone context for parallel or use Arc<Mutex<_>>
+        (r#move, -negamax(updated_board, depth - 1, f64::NEG_INFINITY, f64::INFINITY, zobrist_table, &mut ctx)) // clone context for parallel or use Arc<Mutex<_>>
     }).collect();
 
     // TODO: if scores are equal, do eval and take the earlier, better move
     scores_by_move.into_iter().max_by(|l, r| l.1.partial_cmp(&r.1).unwrap())
 }
 
-fn negamax(board: Board, depth: u8, zobrist_table: &ZobristTable, ctx: &mut OptimizationContext) -> f64 {
+fn negamax(board: Board, depth: u8, mut alpha: f64, beta: f64, zobrist_table: &ZobristTable, ctx: &mut OptimizationContext) -> f64 {
     // Lookup transposition table
-    if let Some(score) = ctx.transposition_table.lookup(board.zobrist_hash, depth) {
-        return *score;
+    if let Some((score, entry_type)) = ctx.transposition_table.lookup(board.zobrist_hash, depth) {
+        match entry_type {
+            EntryType::Exact => {
+                return *score;
+            }
+            EntryType::UpperBound => {
+                if *score >= beta {
+                    return *score;
+                }
+            }
+            EntryType::LowerBound => {
+                if *score <= alpha {
+                    return *score;
+                }
+            }
+        }
     }
 
     if depth == 0 {
         let score = board.evaluate_position_for_current_player();
 
-        ctx.transposition_table.insert(board.zobrist_hash, depth, score);
+        ctx.transposition_table.insert(board.zobrist_hash, depth, score, EntryType::Exact);
 
         return score;
     }
 
     let next_player = board.next_player;
     let moves = board.generate_moves(next_player);
+    let orig_alpha = alpha;
 
     let scores = moves.into_iter().map(|r#move| {
+        if alpha >= beta {
+            return f64::NEG_INFINITY; // TODO: for loop -> abort
+        }
+
+        // King capture is terminal move
         if r#move.is_capture_king(&board) {
             return 200.0;
         }
@@ -54,13 +73,23 @@ fn negamax(board: Board, depth: u8, zobrist_table: &ZobristTable, ctx: &mut Opti
         let mut updated_board = board.clone(); // TODO: get mutable board, make move, unmake move
         updated_board.execute_move(r#move, zobrist_table);
 
-        -negamax(updated_board, depth - 1, zobrist_table, ctx)
+        let score = -negamax(updated_board, depth - 1, -beta, -alpha, zobrist_table, ctx);
+
+        alpha = alpha.max(score);
+
+        score
     });
 
     // TODO: if scores are equal, do eval and take the earlier, better move
     let score = scores.max_by(|l, r| l.partial_cmp(&r).unwrap()).unwrap_or(0.0); // draw evaluates to 0, but this would ignore king taken!!! -> abort early / take into account
 
-    ctx.transposition_table.insert(board.zobrist_hash, depth, score);
+    if score <= orig_alpha {
+        ctx.transposition_table.insert(board.zobrist_hash, depth, score, EntryType::UpperBound);
+    } else if score >= beta {
+        ctx.transposition_table.insert(board.zobrist_hash, depth, score, EntryType::LowerBound);
+    } else {
+        ctx.transposition_table.insert(board.zobrist_hash, depth, score, EntryType::Exact);
+    }
 
     score
 }
--- src/chess/transposition.rs
+++ src/chess/transposition.rs
@@ -3,17 +3,25 @@ use hashbrown::HashMap;
 
 #[derive(Default, Clone)]
 pub struct TranspositionTable {
-    hash_map: HashMap<u64, f64>,
+    hash_map: HashMap<u64, (f64, EntryType)>,
     insert_count: u64,
     lookup_count: u64,
     hit_count: u64,
 }
 
+#[repr(u8)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum EntryType {
+    Exact = 0,
+    UpperBound = 1,
+    LowerBound = 2,
+}
+
 impl TranspositionTable {
     // TODO: add debug counting
     #[inline]
-    pub fn insert(&mut self, hash: u64, depth: u8, score: f64) { // depth we still want to search until evaluation
-        self.hash_map.insert(hash + depth as u64, score);
+    pub fn insert(&mut self, hash: u64, depth: u8, score: f64, entry_type: EntryType) { // depth we still want to search until evaluation
+        self.hash_map.insert(hash + depth as u64, (score, entry_type));
 
         #[cfg(debug_assertions)] {
             self.insert_count += 1;
@@ -21,7 +29,7 @@ impl TranspositionTable {
     }
 
     #[inline]
-    pub fn lookup(&mut self, hash: u64, depth: u8) -> Option<&f64> { // depth we still want to search until evaluation
+    pub fn lookup(&mut self, hash: u64, depth: u8) -> Option<&(f64, EntryType)> { // depth we still want to search until evaluation
         let res = self.hash_map.get(&(hash + depth as u64));
         #[cfg(debug_assertions)] {
             self.lookup_count += 1;
@@ -30,7 +38,7 @@ impl TranspositionTable {
             }
         }
 
-        res // TODO: test behaviour against None
+        res
     }
 }
 
